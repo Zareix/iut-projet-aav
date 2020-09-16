2d782f34946ade52b7b86c417ea171680e98370c
Mohamed772 <48584065+Mohamed772@users.noreply.github.com>
2020-09-16 14:58:35 +0200

    debut

--- src/SacADos.java
+++ src/SacADos.java
@@ -1,16 +1,57 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SacADos {
+	ArrayList<Item> contenu;
+	ArrayList<Item> listeObjets;
+	float poidsMaximal;
+	
 	public SacADos() {
-		// TODO Stub du constructeur généré automatiquement
+		contenu = new ArrayList<Item>();
 	}
 	
 	public SacADos(String chemin, float poidsMaximal) {
-		//TODO
+		contenu = new ArrayList<Item>();
+		listeObjets = new ArrayList<Item>();
+		readItems(chemin);
+		this.poidsMaximal = poidsMaximal;
+
 	}
+	
+	private static void readItems(String chemin) {
+		try {
+			Scanner file = new Scanner(new FileInputStream(chemin));
+			while (file.hasNextLine()){
+				Scanner scLine = new Scanner(file.nextLine());
+				String line ="";
+				String name="";float poids;float valeur;
+				line = scLine.nextLine();
+				
+				for(int i=0; i< line.length();++i) {
+					if (line[i] ==';') {
+						
+					}
+				}
+				
+				
+				Item it = new Item();
+				
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
 	
 	@Override
 	public String toString() {
-		// TODO Stub de la méthode généré automatiquement
+		// TODO Stub de la mï¿½thode gï¿½nï¿½rï¿½ automatiquement
 		return super.toString();
 	}
 	
