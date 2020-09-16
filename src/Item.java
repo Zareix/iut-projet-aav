3b4483b299de8c573800b7326b1dd59884e2f256
Zareix <raphcatarino@gmail.com>
2020-09-16 11:51:34 +0200

    nouvelles classes

---
+++ src/Item.java
@@ -0,0 +1,19 @@
+
+public class Item {
+	private float valeur;
+	private float poids;
+	
+	public Item(float v, float p) {
+		this.valeur = v;
+		this.poids = p;
+	}
+	
+	public float getPoids() {
+		return poids;
+	}
+	
+	public float getValeur() {
+		return valeur;
+	}
+	
+}
