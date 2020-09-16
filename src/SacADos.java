bdbae949738765fd5c78603400ea003b0fa6342f
Zareix <raphcatarino@gmail.com>
2020-09-16 11:36:50 +0200

    First class and create items.txt

---
+++ src/SacADos.java
@@ -0,0 +1,20 @@
+
+public class SacADos {
+	public SacADos() {
+		// TODO Stub du constructeur généré automatiquement
+	}
+	
+	public SacADos(String chemin, float poidsMaximal) {
+		//TODO
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Stub de la méthode généré automatiquement
+		return super.toString();
+	}
+	
+	public void resoudre() {
+		//TODO
+	}
+}
